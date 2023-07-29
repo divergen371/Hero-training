0533d2a13adfdf8c06d9c73ce822b2c55241b257
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-29 17:05:39 +0900

    :alembic: 複数の条件を評価する

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    public bool weaponEquipped = true;
-    public string weaponType = "Longsword";
+    public bool pureOfHeart = true;
+    public bool hasSecretIncantation = false;
+    public string rareItem = "The Soul of a Fanatic";
 
     void Start()
     {
-        if (weaponEquipped
-            && weaponType == "Longsword") 
-        {
-                Debug.Log("女王陛下バンザイ");
-        }
-        else
-        {
-            Debug.Log("素手では鎧武者には勝てませぬぞ…。");
-	    }
+        OpenTresureChamber();
     }
 
 
     void Update()
     {
         
+    }
+
+    public void OpenTresureChamber()
+    {
+        if (pureOfHeart && rareItem == "The Soul of a Fanatic")
+        {
+            if (!hasSecretIncantation)
+            {
+                Debug.Log("善き心を持つものよ、知識を携えてまた来るが良い。");
+            }
+            else
+            {
+                Debug.Log("真の勇者よ、汝にこそこの宝は相応しい。");
+	        }
+        }
+        else
+        {
+            Debug.Log("資格を得てまた来るが良い。");
+        }
     }
 }
