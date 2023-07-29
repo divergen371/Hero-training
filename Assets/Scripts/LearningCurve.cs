4d848839b4fdc296d4108b12ba9b36ab5e3a75a3
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-29 11:27:23 +0900

    異なる型の演算がエラーとなる例

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,11 +6,18 @@ public class LearningCurve : MonoBehaviour
 {
     public int currentAge = 30;
     public int addedAge = 1;
+
+    public float pi = 3.1415f;
+    public string firstName = "Harrison";
+    public bool isAuthor = true;
     // Start is called before the first frame update
     void Start()
     {
         ComputeAge();
+        Debug.Log($"文字列には{firstName}のような変数を挿入できる");
+        Debug.Log(firstName * pi);
     }
+
 
     void ComputeAge()
     {
