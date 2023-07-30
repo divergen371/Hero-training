a49491ae1a06d2e9a75e207e543f55f11b8e67f0
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-30 16:55:46 +0900

    while

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -6,25 +6,14 @@ public class LearningCurve : MonoBehaviour
 {
     void Start()
     {
-        int wallet = 100;
-        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
-        {
-            {"Potion", 100 },
-            {"Antidote", 150 },
-            {"Aspirin", 200 },
-        };
-        foreach (KeyValuePair<string, int>  kpv in itemInventory)
-        {
-            if (wallet >= kpv.Value)
-            {
-                Debug.LogFormat("{0}  can be bought.", kpv.Key);
-            }
-            else
-            {
-                Debug.LogFormat("I can't afford {0} because I don't have enough on hand.", kpv.Key);
-	        }
-        }
+        int playerLives = 5;
 
+        while (playerLives > 0)
+        {
+            Debug.Log("player still alive");
+            playerLives--;
+	}
+        Debug.Log("The player's life has ended.");
     }
 
 
