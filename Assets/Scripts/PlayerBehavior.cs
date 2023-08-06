1e409380fe618706cbd03d35a6e0f218b9f6c688
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-08-06 18:24:33 +0900

    Rigidbodyコンポーネントへのアクセスと操作

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,7 +9,12 @@ public class PlayerBehavior : MonoBehaviour
 
     private float vInput;
     private float hInput;
-    // Start is called before the first frame update
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,7 +22,16 @@ public class PlayerBehavior : MonoBehaviour
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        //this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        //this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
