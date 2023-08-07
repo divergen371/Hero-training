c7230a760f4908d6f37654aecd917457d4571962
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-08-07 11:32:51 +0900

    レイヤーマスクを使ってジャンプ判定

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,14 +6,19 @@ public class PlayerBehavior : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
 
     private float vInput;
     private float hInput;
     private Rigidbody _rb;
+    private CapsuleCollider _col;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -33,5 +38,17 @@ public class PlayerBehavior : MonoBehaviour
 
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+
     }
+	private bool IsGrounded()
+	{
+	Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.center.z);
+	bool grounded = Physics.CheckCapsule(_col.bounds.center,capsuleBottom, distanceToGround, groundLayer ,QueryTriggerInteraction.Ignore);
+	return grounded;
+	}
 }
