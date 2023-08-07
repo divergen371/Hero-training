67bd73d3a77414cceb25d50dc7374bfd338f2b35
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-08-07 11:51:59 +0900

    弾丸の発射機構を作る

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,6 +9,8 @@ public class PlayerBehavior : MonoBehaviour
     public float jumpVelocity = 5f;
     public float distanceToGround = 0.1f;
     public LayerMask groundLayer;
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
 
     private float vInput;
     private float hInput;
@@ -36,19 +38,40 @@ public class PlayerBehavior : MonoBehaviour
         Vector3 rotation = Vector3.up * hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
-        _rb.MoveRotation(_rb.rotation * angleRot);
+        _rb.MovePosition(this.transform.position + 
+	        this.transform.forward * 
+	            vInput * Time.fixedDeltaTime);
+        
+	    _rb.MoveRotation(_rb.rotation * angleRot);
 
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
         }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet,
+                this.transform.position + new Vector3(1, 0, 0),
+                    this.transform.rotation) as GameObject;
+
+            Rigidbody bulletRB =
+                newBullet.GetComponent<Rigidbody>();
+
+            bulletRB.velocity = this.transform.forward *
+                bulletSpeed;
+        }
     }
 	private bool IsGrounded()
 	{
-	Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.center.z);
-	bool grounded = Physics.CheckCapsule(_col.bounds.center,capsuleBottom, distanceToGround, groundLayer ,QueryTriggerInteraction.Ignore);
+	Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, 
+	    _col.bounds.min.y, 
+	        _col.center.z);
+	bool grounded = Physics.CheckCapsule(_col.bounds.center,
+	    capsuleBottom, 
+	        distanceToGround, 
+	            groundLayer,
+	                QueryTriggerInteraction.Ignore);
 	return grounded;
 	}
 }
