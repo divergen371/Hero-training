ff29f0be0dc365ac851832e86fabd3b6b320bd0b
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-08-09 17:53:34 +0900

    敵を移動させる

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,9 +1,50 @@
 using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform patrolRoute;
+    public List<Transform> locations;
+
+    private int locationIndex = 0;
+    private NavMeshAgent agent; 
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MovoToNextPatrolLocation();
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MovoToNextPatrolLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in  patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MovoToNextPatrolLocation()
+    {
+        if (locations.Count == 0)
+        {
+            return;
+        }
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
